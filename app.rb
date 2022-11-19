f4b5001324348d8febd0e451a0b637b3ca456338
Hebron George <hebrontgeorge@gmail.com>
2022-11-19 01:33:19 -0500

    fix spacing

--- app.rb
+++ app.rb
@@ -1,74 +1,74 @@
 def update_ip_address!
-	current_ip  = get_current_ip
-	previous_ip = get_previous_ip
+  current_ip  = get_current_ip
+  previous_ip = get_previous_ip
 
-	if current_ip != previous_ip
-		update_dns!(previous_ip, current_ip)
-	else
-		log_no_change
-	end	
+  if current_ip != previous_ip
+    update_dns!(previous_ip, current_ip)
+  else
+    log_no_change
+  end 
 end
 
 def project_dir
-	File.expand_path(File.dirname(__FILE__)) + "/"
+  File.expand_path(File.dirname(__FILE__)) + "/"
 end
 
 def get_current_ip
-	`curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'`.strip
+  `curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'`.strip
 end
 
 def get_previous_ip
-	File.open(project_dir + "previous_ip").read
+  File.open(project_dir + "previous_ip").read
 end
 
 def log(message)
-	current_time = Time.now
-	message = current_time.to_s + "\t" + message.to_s
+  current_time = Time.now
+  message = current_time.to_s + "\t" + message.to_s
 
-	File.open(project_dir + "logs/dynamic_dns.log", "a+") { |f| f.puts(message) }
+  File.open(project_dir + "logs/dynamic_dns.log", "a+") { |f| f.puts(message) }
 end
 
 def log_ip_change(previous_ip, current_ip)
-	log("Old IP used to be: #{previous_ip} \t New IP is: #{current_ip}")
+  log("Old IP used to be: #{previous_ip} \t New IP is: #{current_ip}")
 end
 
 def log_no_change
-	log("Job ran with no IP address change")
+  log("Job ran with no IP address change")
 end
 
 def log_error(message)
-	log("Error: #{message}")
+  log("Error: #{message}")
 end
 
 def update_previous_ip!(previous_ip, new_ip)
-	log_ip_change(previous_ip, new_ip)
-	File.open(project_dir + 'previous_ip', 'w').write(new_ip)
+  log_ip_change(previous_ip, new_ip)
+  File.open(project_dir + 'previous_ip', 'w').write(new_ip)
 end
 
 def update_dns!(previous_ip, new_ip)
-	host          = ENV['HOST']
+  host          = ENV['HOST']
   domain_name   = ENV['DYNAMIC_DNS_DOMAIN_NAME']
-	ddns_password = ENV['DDNS_PASSWORD']
+  ddns_password = ENV['DDNS_PASSWORD']
 
-	ensure_valid_variables!(host, domain_name, ddns_password, new_ip)
-	
-	url = "https://dynamicdns.park-your-domain.com/update?host=#{host}&domain=#{domain_name}&password=#{ddns_password}&ip=#{new_ip}"
+  ensure_valid_variables!(host, domain_name, ddns_password, new_ip)
+  
+  url = "https://dynamicdns.park-your-domain.com/update?host=#{host}&domain=#{domain_name}&password=#{ddns_password}&ip=#{new_ip}"
   require 'net/https'
 
-	url  = URI.parse(url)
-	http = Net::HTTP.new(url.host, url.port)
-	req  = Net::HTTP::Get.new(url.path)
+  url  = URI.parse(url)
+  http = Net::HTTP.new(url.host, url.port)
+  req  = Net::HTTP::Get.new(url.path)
 
-	http.use_ssl = url.port == 443
-	res = http.start { |http| http.request(req) }
-	update_previous_ip!(previous_ip, new_ip)
+  http.use_ssl = url.port == 443
+  res = http.start { |http| http.request(req) }
+  update_previous_ip!(previous_ip, new_ip)
 end
 
 def ensure_valid_variables!(host, domain_name, ddns_password, ip_address)
-	if host.nil? || domain_name.nil? || ddns_password.nil? || ip_address.nil?
-		log_error("Missing env vars: Host: #{host}, Domain Name: #{domain_name}, DDNS Pass: #{ddns_password}, New IP Address: #{ip_address}")
-		exit
-	end
+  if host.nil? || domain_name.nil? || ddns_password.nil? || ip_address.nil?
+    log_error("Missing env vars: Host: #{host}, Domain Name: #{domain_name}, DDNS Pass: #{ddns_password}, New IP Address: #{ip_address}")
+    exit
+  end
 end
 
 update_ip_address!
